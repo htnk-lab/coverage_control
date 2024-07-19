42b4afa78bc12683962a76f120516f37ad3aff08
Minato Miyashita <minato0818konami@gmail.com>
2024-07-19 19:09:50 +0900

    single segment

--- cbf_pcc/cbf_pcc/field_cbf_optimizer.py
+++ cbf_pcc/cbf_pcc/field_cbf_optimizer.py
@@ -6,10 +6,10 @@ import rclpy
 from cbfpy.cbf import CBFBase
 from cbfpy.cbf_qp_solver import CBFNomQPSolver
 from geometry_msgs.msg import Point, Pose, Twist, Vector3
-from numpy.typing import NDArray
+from rcl_interfaces.msg import ParameterDescriptor, ParameterType
 from rclpy.node import Node
 from std_msgs.msg import Header
-from sympy import Matrix, Symbol, Tuple, lambdify
+from sympy import Matrix, Symbol, lambdify, symbols
 from visualization_msgs.msg import Marker
 
 from .coverage_utils.utils import get_color_rgba
@@ -17,49 +17,54 @@ from .coverage_utils.utils import get_color_rgba
 
 class WallCBF(CBFBase):
     def __init__(self) -> None:
-        self.x = Matrix(Symbol("x y", real=True))
+        self.x = Matrix(symbols("x, y", real=True))
         self.sign = Symbol("sign_", real=True)
+        # 初期化
+        self.segment_p1 = np.zeros(2)
+        self.segment_p2 = np.zeros(2)
+
+    def _alpha(self, h: List[float]) -> List[float]:
+        return [0.1*_h for _h in h]
 
     def set_parameters(self, d: float, keep_inside: bool = True) -> None:
         self.d = d
         self.keep_inside = keep_inside
-        sign = 1 if keep_inside else -1
+        # sign = 1 if keep_inside else -1
 
-        cbf = self.sign * (self.d - self.distance_to_segment(self.x))
+        cbf = self.sign * (self.d**2 - self.distance_to_segment(self.x))
         self._calc_dhdx = lambdify([self.x, self.sign], cbf.diff(self.x))
         self._calc_h = lambdify([self.x, self.sign], cbf)
 
     def get_parameters(self) -> Tuple[float, bool]:
-        return (self.d, self.keep_inside)
+        return self.d, self.keep_inside
 
     def calc_constraints(self, agent_position: np.ndarray, x_wall: np.ndarray, y_wall: np.ndarray) -> None:
-        """制約行列 G と制約値 h を計算"""
         self.G = []
         self.h = []
-
-        for i in range(len(x_wall) - 1):
+        for i in range(len(x_wall)-1):
+        # for i in range(2):
             p1 = np.array([x_wall[i], y_wall[i]])
             p2 = np.array([x_wall[i + 1], y_wall[i + 1]])
-            self.segment_p1 = p1
-            self.segment_p2 = p2
 
-            # エージェントの位置が線分の内側にあるかどうかをチェック
+            # エージェントの位置が線分の内側にあるか
             if self.inside_segment(agent_position, p1, p2):
                 agent_position = agent_position.flatten()
+
                 sign = 1 if self.keep_inside else -1
 
-                G_segment = self._calc_dhdx(agent_position, sign)
+                G_segment = self._calc_dhdx(agent_position, sign).flatten()
                 h_segment = self._calc_h(agent_position, sign)
 
                 self.G.append(G_segment)
                 self.h.append(h_segment)
+        
+        # print(self.G)
+        # print(self.h)
+        if len(self.G) == 0:
+            self.G = [np.zeros((2,))]
+            self.h = [np.ones((1,))]
 
-        if self.G:
-            self.G = np.array(self.G).reshape(-1, 2)
-            self.h = np.array(self.h).flatten()
-        else:
-            self.G = np.empty((0, 2))
-            self.h = np.empty((0,))
+
 
     def distance_to_segment(self, point: Matrix) -> Symbol:
         x1, y1 = self.segment_p1
@@ -67,10 +72,15 @@ class WallCBF(CBFBase):
         px, py = point
         dx = x2 - x1
         dy = y2 - y1
+        a = -dy
+        b = dx
+        c = -x2*y1 + y2*x1
         d = dx**2 + dy**2
-        u = ((px - x1) * dx + (py - y1) * dy) / d
-        closest_point = Matrix([x1 + u * dx, y1 + u * dy])
-        distance = (closest_point - point).norm()
+        distance = (a*px + b*py + c)**2 / d
+        # u = ((px - x1) * dx + (py - y1) * dy) / d
+        # closest_point = Matrix([x1 + u * dx, y1 + u * dy])
+        # distance = (closest_point - point).norm()
+        # print(distance)
         return distance
 
     def inside_segment(self, agent_position: np.ndarray, p1: np.ndarray, p2: np.ndarray) -> bool:
@@ -78,8 +88,8 @@ class WallCBF(CBFBase):
         if inner_product < 0:
             return False  # 線分の外側にある
 
-        squared_length = (p2[0] - p1[0]) ** 2 + (p2[1] - p1[1]) ** 2
-        if inner_product > squared_length:
+        length = (p2[0] - p1[0]) ** 2 + (p2[1] - p1[1]) ** 2
+        if inner_product >= length:
             return False  # 線分の外側にある
         return True  # 線分の内側にある
 
@@ -92,7 +102,7 @@ class CBFOptimizer:
         self.wall_cbf = WallCBF()
 
         # initialize(must be overwritten)
-        self.set_parameters(1.0, True)  # Example parameters
+        self.set_parameters(0.01, True)
 
     def set_parameters(self, d: float, keep_inside: bool = True) -> None:
         self.wall_cbf.set_parameters(d, keep_inside)
@@ -112,11 +122,15 @@ class CBFOptimizer:
     ) -> Tuple[str, np.ndarray]:
         self._calc_constraints(agent_position, x_wall, y_wall)
         G_list, alpha_h_list = self._get_constraints()
+        # if not alpha_h_list == []
+        self.qp_nom_solver._set_solver_options(feastol=1e-14)
+        # print(G_list)
+        # print(alpha_h_list)
 
         try:
             return self.qp_nom_solver.optimize(nominal_input, self.P, G_list, alpha_h_list)
         except Exception as e:
-            raise e
+            return 0, nominal_input
 
 
 class FieldCBFOptimizer(Node):
@@ -151,7 +165,7 @@ class FieldCBFOptimizer(Node):
             ns="obstacle_marker",
             action=Marker.ADD,
             type=Marker.LINE_LIST,
-            scale=Vector3(x=0.1, y=0.0, z=0.0),
+            scale=Vector3(x=0.03, y=0.0, z=0.0),
             color=get_color_rgba(color="r", alpha=0.7),
             points=[],
         )
@@ -174,20 +188,35 @@ class FieldCBFOptimizer(Node):
 
     def curr_pose_callback(self, msg: Pose) -> None:
         self.curr_pose = msg
+        # self.get_logger().info(f"{self.curr_pose}")
 
     def cmd_vel_nom_callback(self, msg: Twist) -> None:
         cmd_vel_opt = msg
 
         # optimize
         if self.activate_cbf:
-            self.optimizer.set_parameters(d=1.0, keep_inside=self.keep_inside)  # Example parameters
+            self.optimizer.set_parameters(d=0.1, keep_inside=self.keep_inside)
             agent_position = np.array([self.curr_pose.position.x, self.curr_pose.position.y])
             nominal_input = np.array([msg.linear.x, msg.linear.y])
+            # nominal_input = np.array([0.0, 0.0])
 
             # 最適入力を計算
             _, optimal_input = self.optimizer.optimize(nominal_input, agent_position, self.x_wall, self.y_wall)
-            if optimal_input is not None:  # 最適化が成功した場合のみ更新
-                cmd_vel_opt.linear = Vector3(x=float(optimal_input[0]), y=float(optimal_input[1]))
+            # self.get_logger().info(f"{self.optimizer.wall_cbf.h}")
+            # self.get_logger().info(f"{self.optimizer.wall_cbf.get_constraints()}")
+            self.get_logger().info(f"p1:{self.optimizer.wall_cbf.segment_p1}")
+            self.get_logger().info(f"p2:{self.optimizer.wall_cbf.segment_p2}")
+            G,alpha_h=self.optimizer.wall_cbf.get_constraints()
+            if isinstance(G,list):
+                G=np.array(G)
+                alpha_h=np.array(alpha_h)
+                # self.get_logger().info(f":{G@optimal_input}")
+                # self.get_logger().info(f"h:{-alpha_h}")
+
+
+            # self.optimizer.optimize(nominal_input, agent_position, self.x_wall, self.y_wall)
+            # cmd_vel_opt.linear = Vector3(x=float(nominal_input[0]), y=float(nominal_input[1]))
+            cmd_vel_opt.linear = Vector3(x=float(optimal_input[0]), y=float(optimal_input[1]))
 
         self.cmd_vel_opt_pub.publish(cmd_vel_opt)
 
